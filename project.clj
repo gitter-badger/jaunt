9203f3e8a84bcfa908c4fcd1ca0f69f21240d01b
Reid 'arrdem' McKenzie <me@arrdem.com>
2016-02-29 13:58:23 -0600

    Use the official faster cljfmt

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :java-source-paths ["src/jvm" "test/java"]
   :test-paths        ["test/clojure"]
   :resource-paths    ["src/resources"]
-  :profiles {:dev {:plugins [[me.arrdem/lein-cljfmt "0.4.5"]]
+  :profiles {:dev {:plugins [[lein-cljfmt "0.4.1"]]
                    :cljfmt  {:indents {fn*                 [[:inner 0]]
                                        as->                [[:inner 0]]
                                        with-debug-bindings [[:inner 0]]
